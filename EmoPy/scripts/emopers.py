3b07d44a30734109a7567e08a82e1df183d1d223
patrickw42 <65424873+patrickw42@users.noreply.github.com>
2021-11-24 16:21:29 -0500

    implemented closing out of emopers.py from the js file. added dummy data videos for testing, until the xls array is implemented

--- EmoPy/scripts/emopers.py
+++ EmoPy/scripts/emopers.py
@@ -14,5 +14,24 @@ def runScript():
     flag = request.form['param']
     return intervention.get_emotion_from_camera(flag)
 
+
+#####Need to implement ajax call to change url to "/close" when we want to close 
+## emopers.py
+
+@app.get('/shutdown')
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
+
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+    
+
+
+#####
 if __name__ == "__main__":
 	app.run()
--- EmoPy/scripts/static/script.js
+++ EmoPy/scripts/static/script.js
@@ -1,15 +1,25 @@
 $( document ).ready(function() {
 	    console.log(emotion);
-	var data = ['static/intervention_data/happy1.mp4','static/intervention_data/happy2.mp4','static/intervention_data/happy3.mp4','static/intervention_data/happy4.mp4'];
+
+	var happy_data = ['static/intervention_data/happy1.mp4','static/intervention_data/happy2.mp4','static/intervention_data/happy3.mp4','static/intervention_data/happy4.mp4'];
+    var motivating_data = ['static/intervention_data/motivating1.mp4', 'static/intervention_data/motivating2.mp4', 'static/intervention_data/motivating3.mp4', 'static/intervention_data/motivating4.mp4',]
 
 	//changing the default image to captured image
 	$('#user').attr("src","static/image_data/image.jpg");
 
 	if(emotion == 'Happy'){
-		//display motivational quote
-		window.close();
+		//display motivational quote video
+		loadAnotherVideo(motivating_data)
+//update url to trigger flask to close emopers.py after video finished 
+//playing (STILL NEED TO IMPLEMENT BROWSER shutdown)
+		$('video')[0].addEventListener('ended',closeHandler,false);
+	    function closeHandler(e) {
+	        location.assign('/shutdown');
+	        window.close();
+	    }    
+//		########################################
 	}else{
-		loadAnotherVideo(data)
+		loadAnotherVideo(happy_data)
 			//Run the script after video finished playing
 		$('video')[0].addEventListener('ended',myHandler,false);
 	    function myHandler(e) {
@@ -33,14 +43,27 @@ $( document ).ready(function() {
 			if (confirm('Do you want to continue watching video?')) {
 				$('#right-container').html('<img id="follow" src="static/image_data/followUp.jpg" />');
 				$('#follow').delay(3000).queue(function(n) {
-				 	loadAnotherVideo(data)
+				 	loadAnotherVideo(happy_data)
+				 	//after 2nd video we close program
+				 	$('video')[0].addEventListener('ended',closeHandler,false);
+	                function closeHandler(e) {
+	                window.close();
+	                location.assign('/shutdown');
+	              
+	    }  
+				 	
 				});
 			} else {
-			  window.close();
+			      window.close();  //doesn't seem to be closing browser			     
+			      location.assign('/shutdown');
+			    
 			}
 		}else{
 			$('#message').html("Looks like your emotion changed from Angry to Happy!");
 			$('#right-container').html('<img id="follow" src="static/image_data/followUp.jpg"/>');
+	     	time.sleep( 5 );  //after 20 seconds shutdown 
+			location.assign('/shutdown');
+			
 		}
 	}
 
