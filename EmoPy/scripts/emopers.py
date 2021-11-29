40beddbe2d6e26f71def0b2a12fe54d6c61d5bbf
patrickw42 <65424873+patrickw42@users.noreply.github.com>
2021-11-29 17:58:45 -0500

    implemented closing browser for chrome. need to test on mac

--- EmoPy/scripts/emopers.py
+++ EmoPy/scripts/emopers.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 app = Flask(__name__, static_folder='static', template_folder='templates',)
 import threading, webbrowser
 import intervention
+import os
+import platform
 
 @app.route("/")
 def index():
@@ -20,8 +22,18 @@ def shutdown():
     if shutdown_func is None:
         raise RuntimeError('Not running werkzeug')
     shutdown_func()
+    ##this closes Chrome chrome browser on windows
+    if platform.system() == 'Windows':
+        os.system("taskkill /im chrome.exe /f")
+    #for close chrome browser for mac os (NEED TO CONFIRM THIS WORKS....)
+    if platform.system() == 'Darwin' :
+        os.system("killall -9 'Google Chrome'")
+        #close hrome browser for Linux os (NEED TO CONFIRM THIS WORKS....)
+    if platform.system() == 'Linux' :
+        os.system("pkill "+ "chrome.exe") 
     return "shutdown"
     
 if __name__ == "__main__":
+    
     threading.Timer(1.25, lambda: webbrowser.open("http://127.0.0.1:5000/") ).start()
     app.run()
