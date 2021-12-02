a3393fd4badfefd808d737a00228c1810907190f
Rinku K Kaneria <rinkubathani@gmail.com>
2021-12-01 23:58:41 -0500

    Update Emoper's.sh

--- Emoper's.sh
+++ Emoper's.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-python3 ~/Desktop/30_emopers/emopers/EmoPy/scripts/emopers.py
-#python3 ~/Desktop/New_emopers/emopers/EmoPy/scripts/emopers.py
+python3 ~/Desktop/New_emopers/emopers/EmoPy/scripts/emopers.py
 read -p "Press enter to continue"
